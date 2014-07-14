d904a403679dd87c41dd9f67a8acf025580ed957
Brendan McMahon <bernos@gmail.com>
2014-07-14 17:13:51 +1000

    moved all iput extensions into single extension class

--- Nancy.TwitterBootstrap/Extensions/InputExtensions.cs
+++ Nancy.TwitterBootstrap/Extensions/InputExtensions.cs
@@ -1,8 +1,10 @@
-﻿using Nancy.TwitterBootstrap.Models;
+﻿using System;
+using Nancy.TwitterBootstrap.Models;
 using System.Collections.Generic;
 
 namespace Nancy.TwitterBootstrap.Extensions
 {
+    // datetime, datetime-local, date, month, time, week, tel, 
     public static class InputExtensions
     {
         public static string Input(this BootstrapRenderer renderer, string name, object value, string type, object htmlAttributes = null)
@@ -14,8 +16,53 @@ namespace Nancy.TwitterBootstrap.Extensions
                 { "name", name },
                 { "attributes", attributes.ToString() },
                 { "type", type },
-                { "value", value == null ? string.Empty : value.ToString() }
+                { "value", value == null ? String.Empty : value.ToString() }
             });
+        }
+
+        public static string Color(this BootstrapRenderer renderer, string name, string value,
+            object htmlAttributes = null)
+        {
+            return Input(renderer, name, value, "color", htmlAttributes);
+        }
+
+        public static string Email(this BootstrapRenderer renderer, string name, string value, object htmlAttributes = null)
+        {
+            return Input(renderer, name, value, "email", htmlAttributes);
+        }
+
+        public static string Number(this BootstrapRenderer renderer, string name, string value,
+            object htmlAttributes = null)
+        {
+            return Input(renderer, name, value, "number", htmlAttributes);
+        }
+
+        public static string Password(this BootstrapRenderer renderer, string name, string value, object htmlAttributes = null)
+        {
+            return Input(renderer, name, value, "password", htmlAttributes);
+        }
+
+        public static string Search(this BootstrapRenderer renderer, string name, object value,
+            object htmlAttributes = null)
+        {
+            return Input(renderer, name, value, "search", htmlAttributes);
+        }
+
+        public static string TelephoneNumber(this BootstrapRenderer renderer, string name, object value,
+            object htmlAttributes = null)
+        {
+            return Input(renderer, name, value, "tel", htmlAttributes);
+        }
+
+        public static string TextBox(this BootstrapRenderer renderer, string name, object value, object htmlAttributes = null)
+        {
+            return Input(renderer, name, value, "text", htmlAttributes);
+        }
+
+        public static string Url(this BootstrapRenderer renderer, string name, string value,
+            object htmlAttributes = null)
+        {
+            return Input(renderer, name, value, "url", htmlAttributes);
         }
     }
 }
